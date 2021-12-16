c31d103be4a6c3a9acabc66b994b9b29f2f76f20
UsrBern <bernardo.pinninghoff@usm.cl>
2021-12-16 17:27:48 -0500

    VICOTORY

--- tarea4.py
+++ tarea4.py
@@ -1,230 +1,214 @@
-import threading as thrd
-import random as rd
+import threading
+import random
 import time
 from datetime import datetime, timedelta
 
-global maximo
-global montana
-global casa
-global barco
-global tiro
-global p_montana
-global p_casa
-global p_barco
-global p_tiro
 
-threads = []
-textzonacomun = []
-textmontana = []
-textcasa = []
-textbarco = []
-textblanco = []
-textsalida = []
+global f_ZonaComun
+global f_MontanaRusa
+global f_CasaTerror
+global f_BarcoPirata
+global f_TiroBlanco
+global f_Salida
+
+global textZonaComun
+
+global MontanaRusa
+global CasaTerror
+global BarcoPirata
+global TiroBlanco
+
+class Juego:
+    nombre = ""
+    capacidadFila = 0
+    duracion = 0
+    capacidadJuego = 0
+    lock = threading.Lock()
+    fila = 0 # cantidad de personas en la fila actualmente
+
+    def __init__(self,nombre,capacidadFila,duracion,capacidadJuego):
+        self.nombre = nombre
+        self.capacidadFila = capacidadFila
+        self.duracion = duracion
+        self.capacidadJuego = capacidadJuego
+
+
+    def jugar(self,p,time_in):
+        print(p.nombre+" has entered queue for "+self.nombre+"\n")
+        s_juego = threading.BoundedSemaphore(self.capacidadJuego)
+
+        s_juego.acquire()
+        self.fila += 1
+
+        while (True):
+            if self.fila >= self.capacidadJuego:
+                break
+        
+        self.lock.acquire()
+        self.log(p.nombre+", "+time_in+", "+datetime.now().strftime("%H:%M:%S")+"\n")
+        self.lock.release()
+        time.sleep(self.duracion)
+        self.fila -= self.capacidadJuego
+    
+    def log(self, string):
+        global f_MontanaRusa
+        global f_CasaTerror
+        global f_BarcoPirata
+        global f_TiroBlanco
+
+        if self.nombre == "Montaña Rusa":
+            f_MontanaRusa = open("MontañaRusa.txt","a")
+            f_MontanaRusa.write(string)
+            f_MontanaRusa.close()
+        elif self.nombre == "Casa del Terror":
+            f_CasaTerror = open("CasaTerror.txt","a")
+            f_CasaTerror.write(string)
+            f_CasaTerror.close()
+        elif self.nombre == "Barco Pirata":
+            f_BarcoPirata = open("BarcoPirata.txt","a")
+            f_BarcoPirata.write(string)
+            f_BarcoPirata.close()
+        elif self.nombre == "Tiro al Blanco":
+            f_TiroBlanco = open("TiroBlanco.txt","a")
+            f_TiroBlanco.write(string)
+            f_TiroBlanco.close()
+        else:
+            print("Error de nombre de juego.\n")
+
+
 
 class Persona:
-
     id = 0
-    nombre = "Persona"+str(id)
-    eleccion = ""
+    nombre = ""
 
     def __init__(self,id):
         self.id = id
+        self.nombre = "Persona_"+str(id)
 
-    def eleccion(self):
-        elecciones = ["montaña rusa", "casa de terror", "barco pirata", "tiro al blanco"]
-        self.eleccion = rd.choice(elecciones)
-        return self.eleccion
+def t_persona(i):
+    global f_ZonaComun
+    global f_Salida
 
+    global textZonaComun
 
-def zona_comun(p):
-    global textzonacomun
-    sem_zona.acquire()
-    juego = p.eleccion()
-    textzonacomun.append(p.nombre+", "+datetime.now().strftime("%H:%M:%S")+", "+juego+", ")
-    return juego
+    global MontanaRusa
+    global CasaTerror
+    global BarcoPirata
+    global TiroBlanco
 
+    s_FilaMR = threading.BoundedSemaphore(MontanaRusa.capacidadFila)
+    s_FilaCT = threading.BoundedSemaphore(CasaTerror.capacidadFila)
+    s_FilaBP = threading.BoundedSemaphore(BarcoPirata.capacidadFila)
+    s_FilaTB = threading.BoundedSemaphore(TiroBlanco.capacidadFila)
 
-def montana_rusa(p):
-    t_fila = datetime.now().strftime("%H:%M:%S")
-
-    global montana
-    global p_montana
-    global textmontana
-
-    while(True):
-        if not lock0.locked():
-            if montana == 1:
-                lock0.acquire()
-                montana = 0
-                lock0.release()
-                while(len(p_montana)>=10):
-                    sem_montana.acquire()
-                file2.write(p.nombre+", "+t_fila+", "+datetime.now().strftime("%H:%M:%S"))
-                p_montana.append(p)
-                time.sleep(5)
-                p_montana = []
-                sem_montana.release()
-                return 1
-
-
-
-def casa_terrorifica(p):
-    t_fila = datetime.now().strftime("%H:%M:%S")
-
-    global casa
-    global p_casa
-    global textcasa
-    while(True):
-        if not lock1.locked():
-            if casa == 1:
-                lock1.acquire()
-                casa = 0
-                lock1.release()
-                while(len(p_casa)>=2):
-                    sem_casa.acquire()
-                file3.write(p.nombre+", "+t_fila+", "+datetime.now().strftime("%H:%M:%S"))
-                p_casa.append(p)
-                time.sleep(3)
-                p_casa = []
-                sem_casa.release()    
-                return 1
-
-def barco_pirata(p):
-    t_fila = datetime.now().strftime("%H:%M:%S")
-
-    global barco
-    global p_barco
-    global textbarco
-    while(True):
-        if not lock2.locked():
-            if barco == 1:
-                lock2.acquire()
-                barco = 0
-                lock2.release()
-                while(len(p_barco)>=5):
-                    sem_barco.acquire()
-                file4.write(p.nombre+", "+t_fila+", "+datetime.now().strftime("%H:%M:%S"))
-                p_barco.append(p)
-                time.sleep(7)
-                p_barco = []
-                sem_barco.release()
-                return 1
-
-
-def tiro_al_blanco(p):
-    t_fila = datetime.now().strftime("%H:%M:%S")
-    
-    global tiro
-    global p_tiro
-    global textblanco
-    while(True):
-        if not lock3.locked():
-            if tiro == 1:
-                lock3.acquire()
-                tiro = 0
-                lock3.release()
-                while(len(p_tiro)>=1):
-                    sem_tiro.acquire()
-                file5.write(p.nombre+", "+t_fila+", "+datetime.now().strftime("%H:%M:%S"))
-                p_tiro.append(p)
-                time.sleep(7)
-                p_tiro = []
-                sem_tiro.release()
-                return 1
-
-def parque(i):
-    t_juego = datetime.now().strftime("%H:%M:%S")
-    global textzonacomun
-    global textsalida
     p = Persona(i+1)
-    lockC.acquire()
-    juego = zona_comun(p)
-    if juego == "montaña rusa":
-        textzonacomun[i] += t_juego
-        lockC.release()
-        montana_rusa(p)
-    elif juego == "casa de terror":
-        textzonacomun[i] += t_juego
-        lockC.release()
-        casa_terrorifica(p)
-    elif juego == "barco pirata":
-        textzonacomun[i] += t_juego
-        lockC.release()
-        barco_pirata(p)
-    else:
-        textzonacomun[i] += t_juego
-        lockC.release()
-        tiro_al_blanco(p)
+    time_in = ""
 
-    textsalida.append(p.nombre+", "+datetime.now().time())
-    return 1
-    
-    
-    
+    # Persona esta en la zona comun
+    lockZC.acquire()
+    f_ZonaComun = open("ZonaComun.txt","a")
+    eleccion = random.choice(juegos) # Persona elige un juego
+    textZonaComun[i] = p.nombre+", "+datetime.now().strftime("%H:%M:%S")+", "+eleccion+", "
 
-if __name__ == '__main__':
-    file1 = open("ZonaComun.txt","w")
-    file2 = open("MontanaRusa.txt","w")
-    file3 = open("CasaTerror.txt","w")
-    file4 = open("BarcoPirata.txt","w")
-    file5 = open("TiroBlanco.txt","w")
-    file6 = open("Salida.txt","w")
+    # Persona en juego
+    if eleccion == "Montaña Rusa":
+        # Guardar log de Zona Comun
+        s_FilaMR.acquire()
+        time_in = datetime.now().strftime("%H:%M:%S")
+        textZonaComun[i] += time_in + "\n"
+        f_ZonaComun.write(textZonaComun[i])
+        f_ZonaComun.close()
+        lockZC.release()
+        
+        # Juego
+        
+        MontanaRusa.jugar(p,time_in)
+        s_FilaMR.release()
+    elif eleccion == "Casa del Terror":
+        # Guardar log de Zona Comun
+        s_FilaCT.acquire()
+        time_in = datetime.now().strftime("%H:%M:%S")
+        textZonaComun[i] += time_in + "\n"
+        f_ZonaComun.write(textZonaComun[i])
+        f_ZonaComun.close()
+        lockZC.release()
+        
+        #Juego
+        CasaTerror.jugar(p,time_in)
+        s_FilaCT.release()
+    elif eleccion == "Barco Pirata":
+        # Guardar log de Zona Comun
+        s_FilaBP.acquire()
+        time_in = datetime.now().strftime("%H:%M:%S")
+        textZonaComun[i] += time_in + "\n"
+        f_ZonaComun.write(textZonaComun[i])
+        f_ZonaComun.close()
+        lockZC.release()
+        
+        # Juego
+        BarcoPirata.jugar(p,time_in)
+        s_FilaBP.release()
+    elif eleccion == "Tiro al Blanco":
+        # Guardar log de Zona Comun
+        s_FilaTB.acquire()
+        time_in = datetime.now().strftime("%H:%M:%S")
+        textZonaComun[i] += time_in + "\n"
+        f_ZonaComun.write(textZonaComun[i])
+        f_ZonaComun.close()
+        lockZC.release()
+
+        # Juego
+        TiroBlanco.jugar(p,time_in)
+        s_FilaTB.release()
+    # Persona termina el juego
+    print(p.nombre+" out of game, is leaving\n")
+
+    # Persona sale del parque
+    lockS.acquire()
+    f_Salida = open("Salida.txt","a")
+    f_Salida.writelines(p.nombre+", "+datetime.now().strftime("%H:%M:%S")+"\n")
+    f_Salida.close()
+    lockS.release()
 
 
-    sem_zona = thrd.BoundedSemaphore(150)
-    sem_montana = thrd.BoundedSemaphore(10)
-    sem_casa = thrd.BoundedSemaphore(8)
-    sem_barco = thrd.BoundedSemaphore(15)
-    sem_tiro = thrd.BoundedSemaphore(5)
-
-    lock0 = thrd.Lock()
-    lock1 = thrd.Lock()
-    lock2 = thrd.Lock()
-    lock3 = thrd.Lock()
-    lockC = thrd.Lock()
-
-    montana = 0
-    casa = 0
-    barco = 0
-    tiro = 0
-
-    p_montana = []
-    p_barco = []
-    p_casa = []
-    p_tiro = []
-
-    for i in range(10):
-        t = thrd.Thread(target=parque, args=(i,))
-        threads.append(t)
-        t.start()
-        print("Thread "+str(i)+" created.\n")
-
-    for t in threads:
-        t.join()
-        print("Threads " +  " joined.\n")
 
 
-    for i in textzonacomun:
-        file1.write(i)
-    
-    for i in textmontana:
-        file2.write(i)
+#------------"MAIN"------------#
+f_ZonaComun = open("ZonaComun.txt","w")
+f_MontanaRusa = open("MontañaRusa.txt","w")
+f_CasaTerror = open("CasaTerror.txt","w")
+f_BarcoPirata = open("BarcoPirata.txt","w")
+f_TiroBlanco = open("TiroBlanco.txt","w")
+f_Salida = open("Salida.txt","w")
 
-    for i in textcasa:
-        file3.write(i)
+f_ZonaComun.close()
+f_TiroBlanco.close()
+f_MontanaRusa.close()
+f_CasaTerror.close()
+f_BarcoPirata.close()
+f_Salida.close()
 
-    for i in textbarco:
-        file4.write(i)
-    
-    for i in textblanco:
-        file5.write(i)
-    
-    for i in textsalida:
-        file6.write(i)
+MontanaRusa = Juego("Montaña Rusa", 10, 5, 10)
+CasaTerror = Juego("Casa del Terror", 8, 3, 2)
+BarcoPirata = Juego("Barco Pirata", 15, 7, 5)
+TiroBlanco = Juego("Tiro al Blanco", 5, 2, 1)
 
-    file1.close()
-    file2.close()
-    file3.close()
-    file4.close()
-    file5.close()
-    file6.close()
+lockZC = threading.Lock()
+lockS = threading.Lock()
+
+
+juegos = ["Montaña Rusa","Casa del Terror","Barco Pirata","Tiro al Blanco"]
+
+textZonaComun = list(range(150))
+
+threads = []
+
+for i in range(150):
+    threads.append(threading.Thread(target=t_persona, args=(i,), daemon=True))
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
+
